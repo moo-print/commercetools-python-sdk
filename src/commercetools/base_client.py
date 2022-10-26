b3bca5202629ace739df142cac05099e6fe28961
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-10-26 12:59:09 +0200

    Rewrite tests to use the new-style API

--- src/commercetools/base_client.py
+++ src/commercetools/base_client.py
@@ -53,7 +53,7 @@ class BaseClient:
 
         # Use environment variables as fallback
         config = {
-            "project_key": project_key,
+            "project_key": project_key or "example-project",
             "client_id": client_id,
             "client_secret": client_secret,
             "url": url,
@@ -231,9 +231,3 @@ class BaseClient:
             sys.platform,
             arch,
         )
-
-
-class Client(BaseClient):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._base_url = f"{self._config['url']}/{self._config['project_key']}/"
